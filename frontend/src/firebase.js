0e0c9a3c7360b05b3603f2a5106df00708291079
23ADR100 <maheshkumarks.23aid@kongu.edu>
2025-12-13 17:23:47 +0530

    Like counter update

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,9 +3,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAiqgJ8A5TC5LhJvAIrh5ffmt666L8N17o",
-  authDomain: "portfolio-likes.firebaseapp.com",
-  projectId: "portfolio-likes",
-  storageBucket: "portfolio-likes.appspot.com",
+  authDomain: "portfolio-likes-50cb6.firebaseapp.com",
+  projectId: "portfolio-likes-50cb6",
+  storageBucket: "portfolio-likes-50cb6.appspot.com",
   messagingSenderId: "82270808648",
   appId: "1:82270808648:web:2cbb9c229c2acda359067d",
 };
