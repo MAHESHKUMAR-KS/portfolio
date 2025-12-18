1f87068652f5cf0a96839b9763e3230c63266d7d
23ADR100 <maheshkumarks.23aid@kongu.edu>
2025-12-18 09:27:21 +0530

    project 5

--- frontend/src/assets/components/Certifications.jsx
+++ frontend/src/assets/components/Certifications.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import ShinyText from './ShinyText.jsx'; // Import the ShinyText component
+import ShinyText from './ShinyText.jsx'; 
 
 const certifications = [
   {
