ba4ea47a3dac521731f8c18b908e98ac83b8ac49
Maheshkumar K.S <maheshsenthil129@gmail.com>
2026-01-09 21:36:49 +0530

    uppdated

--- frontend/src/assets/components/Particles.jsx
+++ frontend/src/assets/components/Particles.jsx
@@ -216,7 +216,7 @@ const Particles = ({
         container.removeChild(gl.canvas);
       }
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    
   }, [
     particleCount,
     particleSpread,
